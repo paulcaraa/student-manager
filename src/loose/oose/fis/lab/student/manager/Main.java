df6eacfee2342faa6dc63946f9e1929c90b5c536
Paul Carabas <carabas_paul@yahoo.com>
2020-03-27 17:14:16 +0200

    SM-1 Solved the comments.

--- src/loose/oose/fis/lab/student/manager/Main.java
+++ src/loose/oose/fis/lab/student/manager/Main.java
@@ -9,24 +9,11 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-<<<<<<< HEAD:src/loose/oose/fis/lab/student/manager/Main.java
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("fxml/login.fxml"));
         primaryStage.setTitle("FIS - Student Manager");
         primaryStage.setScene(new Scene(root, 600, 400));
-=======
-
-    public void start(Stage primaryStage) throws Exception {
-
-        Parent root = FXMLLoader.load(getClass().getResource("fxml/view-students.fxml"));
-
-        primaryStage.setTitle("FIS - Student Manager");
-
-        primaryStage.setScene(new Scene(root, 600, 400));
-
->>>>>>> 156ad7ba24135ed8b1766efe968b87016d75d724:src/loose/oose/fis/lab/student/manager/Main.java
         primaryStage.show();
-
     }
 
 
